4bbc9c9ba135e891a652188d46c406b67a23b004
Everton Santana <101647763+EvertonSantana1@users.noreply.github.com>
2022-08-25 15:37:46 -0300

    Add files via upload
    Tabela, para adicionar N Números inteiros e ter ume média sobre os números fornecido pelo usuário

---
+++ MédiaDeNúmerosInteiros.py
@@ -0,0 +1,11 @@
+notas = []
+soma = 0 
+n = int(input("Entre com o número de notas"))
+for i in range(1,n+1):
+    nota = float(input("Entre com a %dª nota: "%i))
+    notas.append(nota)
+    soma+=nota
+print(notas)
+
+media = soma/n
+print("%.2f"%media)
